070a4eb28d08acbd00dfa4d808514eebe49332a8
Daniyal Ahmad <daniyalahmad3492@gmail.com>
2025-01-09 13:46:49 +0500

    Update insertionsort.cpp

--- all-sort-algorithms/sort_algorithms/insertionsort.cpp
+++ all-sort-algorithms/sort_algorithms/insertionsort.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    int iterations = 0;  // Count iterations
+
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            ++iterations;  // Count each comparison
+            arr[j + 1] = arr[j];
+            --j;
+        }
+        arr[j + 1] = key;
+    }
+
+    cout << "Insertion Sort - Iterations: " << iterations << endl;
+}
 
