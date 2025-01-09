a2627ff690b70b8de2ef68ccd83d15e0d417c8fe
Daniyal Ahmad <daniyalahmad3492@gmail.com>
2025-01-09 13:44:33 +0500

    Update selectionsort.cpp

--- all-sort-algorithms/sort_algorithms/selectionsort.cpp
+++ all-sort-algorithms/sort_algorithms/selectionsort.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+    int iterations = 0;  // Count iterations
+
+    for (int i = 0; i < n - 1; ++i) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; ++j) {
+            ++iterations;  // Count each comparison
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        swap(arr[i], arr[minIdx]);
+    }
+
+    cout << "Selection Sort - Iterations: " << iterations << endl;
+}
 
