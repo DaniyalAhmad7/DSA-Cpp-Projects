41bc7380ca5b7b0372788759a2305c7328127a2c
Daniyal Ahmad <daniyalahmad3492@gmail.com>
2025-01-09 13:45:10 +0500

    Update quicksort.cpp

--- all-sort-algorithms/sort_algorithms/quicksort.cpp
+++ all-sort-algorithms/sort_algorithms/quicksort.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int quickSortIterations = 0;  // Global counter for Quick Sort iterations
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; ++j) {
+        if (arr[j] <= pivot) {
+            ++i;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        ++quickSortIterations;  // Increment iterations for each partition
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
 
