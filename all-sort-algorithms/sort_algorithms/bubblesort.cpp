d1aa42f63887ae97b80f84cf7542a7f8ad8410ef
Daniyal Ahmad <daniyalahmad3492@gmail.com>
2025-01-09 13:47:17 +0500

    Update bubblesort.cpp

--- all-sort-algorithms/sort_algorithms/bubblesort.cpp
+++ all-sort-algorithms/sort_algorithms/bubblesort.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    int iterations = 0;  // Count iterations
+
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            ++iterations;  // Count each comparison
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    cout << "Bubble Sort - Iterations: " << iterations << endl;
+}
 
