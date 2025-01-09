1b461eee76b692f30e76302bddf3e1ac8d751066
Daniyal Ahmad <daniyalahmad3492@gmail.com>
2025-01-09 13:43:14 +0500

    Update sort_algorithms.h

--- all-sort-algorithms/sort_algorithms/sort_algorithms.h
+++ all-sort-algorithms/sort_algorithms/sort_algorithms.h
@@ -1,1 +1,14 @@
+#ifndef SORT_ALGORITHMS_H
+#define SORT_ALGORITHMS_H
+
+#include <vector>
+using namespace std;
+
+void bubbleSort(vector<int>& arr);
+void selectionSort(vector<int>& arr);
+void insertionSort(vector<int>& arr);
+void mergeSort(vector<int>& arr, int left, int right);
+void quickSort(vector<int>& arr, int low, int high);
+
+#endif
 
